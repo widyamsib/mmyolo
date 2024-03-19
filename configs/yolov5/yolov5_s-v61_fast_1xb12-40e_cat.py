f11b255670266d23b10f9293566c85f30ea1f2ea
aqilwahid <aqilwahid@gmail.com>
2024-03-19 14:01:42 +0700

    v1

--- configs/yolov5/yolov5_s-v61_fast_1xb12-40e_cat.py
+++ configs/yolov5/yolov5_s-v61_fast_1xb12-40e_cat.py
@@ -1,27 +1,37 @@
+# Inherit and overwrite part of the config based on this config
 _base_ = 'yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py'
 
-data_root = './data/cat/'
-class_name = ('cat', )
-num_classes = len(class_name)
+data_root = './data/cat/' # dataset root
+class_name = ('cat', ) # dataset category name
+num_classes = len(class_name) # dataset category number
+# metainfo is a configuration that must be passed to the dataloader, otherwise it is invalid
+# palette is a display color for category at visualization
+# The palette length must be greater than or equal to the length of the classes
 metainfo = dict(classes=class_name, palette=[(20, 220, 60)])
 
+# Adaptive anchor based on tools/analysis_tools/optimize_anchors.py
 anchors = [
     [(68, 69), (154, 91), (143, 162)],  # P3/8
     [(242, 160), (189, 287), (391, 207)],  # P4/16
     [(353, 337), (539, 341), (443, 432)]  # P5/32
 ]
-
+# Max training 40 epoch
 max_epochs = 40
+# Set batch size to 12
 train_batch_size_per_gpu = 12
+# dataloader num workers
 train_num_workers = 4
 
+# load COCO pre-trained weight
 load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-v61_syncbn_fast_8xb16-300e_coco/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth'  # noqa
 
 model = dict(
+    # Fixed the weight of the entire backbone without training
     backbone=dict(frozen_stages=4),
     bbox_head=dict(
         head_module=dict(num_classes=num_classes),
-        prior_generator=dict(base_sizes=anchors)))
+        prior_generator=dict(base_sizes=anchors)
+    ))
 
 train_dataloader = dict(
     batch_size=train_batch_size_per_gpu,
@@ -29,7 +39,9 @@ train_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
+        # Dataset annotation file of json path
         ann_file='annotations/trainval.json',
+        # Dataset prefix
         data_prefix=dict(img='images/')))
 
 val_dataloader = dict(
@@ -47,10 +59,13 @@ val_evaluator = dict(ann_file=data_root + 'annotations/test.json')
 test_evaluator = val_evaluator
 
 default_hooks = dict(
+    # Save weights every 10 epochs and a maximum of two weights can be saved.
+    # The best model is saved automatically during model evaluation
     checkpoint=dict(interval=10, max_keep_ckpts=2, save_best='auto'),
     # The warmup_mim_iter parameter is critical.
     # The default value is 1000 which is not suitable for cat datasets.
     param_scheduler=dict(max_epochs=max_epochs, warmup_mim_iter=10),
+    # The log printing interval is 5
     logger=dict(type='LoggerHook', interval=5))
+# The evaluation interval is 10
 train_cfg = dict(max_epochs=max_epochs, val_interval=10)
-# visualizer = dict(vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend')]) # noqa
